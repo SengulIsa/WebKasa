675aeccfca2667112c1b349a6944464acab90258
İsa ŞENGÜL <sengulisa49@icloud.com>
2024-04-21 18:24:44 +0300

    updating order page and edition of categories section

---
+++ src/Components/Category.js
@@ -0,0 +1,28 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
+
+function Category({image,tittle}) {
+    return ( 
+    <Card sx={{ maxWidth: 345,border:'1px solid gray',borderRadius:'20px' }}>
+        <CardActionArea>
+          <CardMedia sx={{width:'100%',height:'90px'}}
+            component="img"
+            height="40"
+            image={image}
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography sx={{fontSize:'14px',fontFamily:'fantasy'}}  variant="h6" component="div">
+              {tittle}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+    </Card>
+       );
+}
+
+export default Category;
--- src/Components/SelectProduct.js
+++ src/Components/SelectProduct.js
@@ -1,11 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {Input,InputAdornment,Button,Container} from '@mui/material';
+import Grid from '@mui/material/Grid';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import '../Styles/OrderPage.css';
+import Category from './Category';
+import HomeKitchen from '../Images/evMutfak.jpeg';
+import HomeLife from '../Images/evYaşam.jpeg';
+import Clothes from '../Images/Giyim.jpeg';
+import BookLibrary from '../Images/KitapKırtasiye.jpeg';
+import Market from '../Images/Market.jpeg';
+import Clean from '../Images/Temizlik.jpeg'; 
 
 const SelectProduct = () => {
+  const [showCategories, setshowCategories] = useState(false);
+  const ShowCategories =()=>{
+    setshowCategories(true);
+    
+  }
   return (
-    <div className='orderComponent'>
-        <button>sdflgkgl</button>
-        <p>dflgkhkgflds</p>
+    <div style={{backgroundColor : 'rgb(218, 236, 237)'}} className='orderComponent '>
+        <Input className='barcodeInput' sx={{border:'1px solid black', borderRadius:'20px',backgroundColor:'white',color:'black',width:'90%',height:'50px',marginLeft:'10px',marginTop:'10px',fontSize:'20px',paddingLeft:'15px'}} disableUnderline='true' placeholder='Klavyeden Barkod Girişi'
+         endAdornment=
+         {
+           <InputAdornment position="start">
+            <CheckCircleIcon sx={{color:'black', fontSize:'50px'}}/>
+           </InputAdornment>
+         }></Input>
+        <Button sx={{border:'1px solid gray',borderRadius:'20px',width:'30%', backgroundColor:'white',color:'black',marginTop:'10px',marginRight:'5px',marginLeft:'3px'}} onClick={ShowCategories}>Kategoriler</Button> 
+        <Button sx={{border:'1px solid gray',borderRadius:'20px',width:'30%', backgroundColor:'white',color:'black',marginTop:'10px',marginRight:'5px'}}>Alt Kategoriler</Button> 
+        <Button sx={{border:'1px solid gray',borderRadius:'20px',width:'30%', backgroundColor:'white',color:'black',marginTop:'10px',}}>Ürünler</Button> 
+        {showCategories ? (
+            <Container maxWidth='lg' sx={{marginTop:'10px'}}>
+              <Grid container spacing={2} >
+          <Grid item xs={12} md={6} lg={4}>
+            <Category
+              image={Market}
+              tittle ="MARKET"
+            />
+          </Grid>
+          <Grid item  xs={12} md={6} lg={4}>
+          <Category
+            image={Clean}
+            tittle ="EV&TEMİZLİK"
+          />
+          </Grid>
+          <Grid item  xs={12} md={6} lg={4}>
+          <Category
+          image={HomeKitchen}
+          tittle ="EV&MUTFAK GEREÇLERİ"
+          />
+          </Grid>
+          <Grid item  xs={12} md={6} lg={4}>
+          <Category
+          image={Clothes}
+          tittle ="GİYİM&AKSESUAR"
+          />
+          </Grid>
+          <Grid item  xs={12} md={6} lg={4}>
+          <Category
+          image={HomeLife}
+          tittle ="EV&YAŞAM"
+          />
+          </Grid>
+          <Grid item  xs={12} md={6} lg={4}>
+          <Category
+          image={BookLibrary}
+          tittle ="KİTAP&KIRTASİYE"
+          />
+          </Grid>
+        </Grid>
+            </Container>
+          
+        ) :(
+            <p></p>
+        )}
     </div>
   )
 }
