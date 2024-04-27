2d15c123ad988287b2a9ab9844b1c595849e97a4
İsa ŞENGÜL <sengulisa49@icloud.com>
2024-04-27 17:43:41 +0300

    Designing Products page and modifying productçjson file

--- src/Components/Category.js
+++ src/Components/Category.js
@@ -2,14 +2,14 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import { CardActionArea} from '@mui/material';
 
 
 function Category({image,tittle}) {
     return ( 
-    <Card sx={{ maxWidth: 345,border:'1px solid gray',borderRadius:'20px' }}>
+    <Card  sx={{ maxWidth: 345,border:'1px solid gray',borderRadius:'20px',height:'140px' }}>
         <CardActionArea>
-          <CardMedia sx={{width:'100%',height:'90px'}}
+          <CardMedia sx={{width:'100%',height:'70px'}}
             component="img"
             height="40"
             image={image}
--- src/Components/SelectProduct.js
+++ src/Components/SelectProduct.js
@@ -1,81 +1,232 @@
-import React, { useState } from 'react';
-import {Input,InputAdornment,Button,Container} from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import { Input, InputAdornment, Button, Container } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import '../Styles/OrderPage.css';
 import Category from './Category';
-import HomeKitchen from '../Images/evMutfak.jpeg';
-import HomeLife from '../Images/evYaşam.jpeg';
-import Clothes from '../Images/Giyim.jpeg';
-import BookLibrary from '../Images/KitapKırtasiye.jpeg';
-import Market from '../Images/Market.jpeg';
-import Clean from '../Images/Temizlik.jpeg'; 
+import Products from './Products';
+import HomeKitchen from '../Images/Products/Kitchen/evMutfak.jpeg';
+import HomeLife from '../Images/Products/Home&Life/evYaşam.jpeg';
+import Clothes from '../Images/Products/Clothing&Accessory/Giyim.jpeg';
+import BookLibrary from '../Images/Products/Book&Stationary/KitapKırtasiye.jpeg';
+import Market from '../Images/Products/Market/Market.jpeg';
+import Clean from '../Images/Products/Home&Clean/Temizlik.jpeg';
+import barkodsuz from '../Images/Products/WithoutBarcode/Barkodsuz.jpeg';
+import axios from 'axios';
 
 const SelectProduct = () => {
-  const [showCategories, setshowCategories] = useState(false);
-  const ShowCategories =()=>{
-    setshowCategories(true);
-    
-  }
+  // Kategorileri ve ürünleri kontrol etmek için state'ler tanımla
+  const [showCategories, setShowCategories] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [productData, setProductData] = useState({
+    names: [],
+    prices: [],
+    codes: [],
+  });
+
+  // Kullanıcı kategori seçtiğinde, ilgili ürünleri almak için etkileşimli olarak useEffect kullan
+  useEffect(() => {
+    // Seçilen kategoriye göre ürünleri getir
+    if (selectedCategory === "Market") {
+      // Axios ile sunucudan ürünleri getir
+      axios.get('http://localhost:3003/Market')
+        .then(result => {
+          // Gelen verileri işle ve uygun şekilde state'e yerleştir
+          const products = result.data;
+          const names = products.map(product => product.name);
+          const prices = products.map(product => product.price);
+          const codes = products.map(product => product.code);
+
+          // Ürün verilerini güncelle
+          setProductData({ names, prices,codes });
+        })
+        .catch(error => {
+          console.error('Ürünler getirilirken hata oluştu:', error);
+        });
+    }
+  }, [selectedCategory]); // selectedCategory değiştiğinde useEffect yeniden çalışır
+
+  useEffect(() => {
+    if (selectedCategory === "home&life") {
+      axios.get('http://localhost:3003/home&life')
+        .then(result => {
+          const products = result.data;
+          const names = products.map(product => product.name);
+          const prices = products.map(product => product.price);
+          const codes = products.map(product => product.code);
+
+          setProductData({ names, prices,codes });
+        })
+        .catch(error => {
+          console.error('Ürünler getirilirken hata oluştu:', error);
+        });
+    }
+  }, [selectedCategory]);
+ 
+  useEffect(() => {
+    if (selectedCategory === "Kitchen") {
+      axios.get('http://localhost:3003/Kitchen')
+        .then(result => {
+          const products = result.data;
+          const names = products.map(product => product.name);
+          const prices = products.map(product => product.price);
+          const codes = products.map(product => product.code);
+
+          setProductData({ names, prices,codes });
+        })
+        .catch(error => {
+          console.error('Ürünler getirilirken hata oluştu:', error);
+        });
+    }
+  }, [selectedCategory]);
+  useEffect(() => {
+    if (selectedCategory === "clothing&accessory") {
+      axios.get('http://localhost:3003/clothing&accessory')
+        .then(result => {
+          const products = result.data;
+          const names = products.map(product => product.name);
+          const prices = products.map(product => product.price);
+          const codes = products.map(product => product.code);
+
+          setProductData({ names, prices,codes });
+        })
+        .catch(error => {
+          console.error('Ürünler getirilirken hata oluştu:', error);
+        });
+    }
+  }, [selectedCategory]);
+
+  useEffect(() => {
+    if (selectedCategory === "book&stationary") {
+      axios.get('http://localhost:3003/book&stationary')
+        .then(result => {
+          const products = result.data;
+          const names = products.map(product => product.name);
+          const prices = products.map(product => product.price);
+          const codes = products.map(product => product.code);
+
+          setProductData({ names, prices,codes });
+        })
+        .catch(error => {
+          console.error('Ürünler getirilirken hata oluştu:', error);
+        });
+    }
+  }, [selectedCategory]);
+
+  useEffect(() => {
+    if (selectedCategory === "home&clean") {
+      axios.get('http://localhost:3003/home&clean')
+        .then(result => {
+          const products = result.data;
+          const names = products.map(product => product.name);
+          const prices = products.map(product => product.price);
+          const codes = products.map(product => product.code);
+
+          setProductData({ names, prices,codes });
+        })
+        .catch(error => {
+          console.error('Ürünler getirilirken hata oluştu:', error);
+        });
+    }
+  }, [selectedCategory]);
+
+  useEffect(() => {
+    if (selectedCategory === "withoutBarcode") {
+      axios.get('http://localhost:3003/withoutBarcode')
+        .then(result => {
+          const products = result.data;
+          const names = products.map(product => product.name);
+          const prices = products.map(product => product.price);
+          const codes = products.map(product => product.code);
+
+          setProductData({ names, prices,codes });
+        })
+        .catch(error => {
+          console.error('Ürünler getirilirken hata oluştu:', error);
+        });
+    }
+  }, [selectedCategory]);
+
+  // Kullanıcının kategori seçimini işle
+  function showProducts(category) {
+    // Seçilen kategoriyi güncelle
+    setSelectedCategory(category);
+    // Kategorileri gizle, ürünleri göster
+    setShowCategories(false);
+  };
+
   return (
-    <div style={{backgroundColor : 'rgb(218, 236, 237)'}} className='orderComponent '>
-        <Input className='barcodeInput' sx={{border:'1px solid black', borderRadius:'20px',backgroundColor:'white',color:'black',width:'90%',height:'50px',marginLeft:'10px',marginTop:'10px',fontSize:'20px',paddingLeft:'15px'}} disableUnderline='true' placeholder='Klavyeden Barkod Girişi'
-         endAdornment=
-         {
-           <InputAdornment position="start">
-            <CheckCircleIcon sx={{color:'black', fontSize:'50px'}}/>
-           </InputAdornment>
-         }></Input>
-        <Button sx={{border:'1px solid gray',borderRadius:'20px',width:'30%', backgroundColor:'white',color:'black',marginTop:'10px',marginRight:'5px',marginLeft:'3px'}} onClick={ShowCategories}>Kategoriler</Button> 
-        <Button sx={{border:'1px solid gray',borderRadius:'20px',width:'30%', backgroundColor:'white',color:'black',marginTop:'10px',marginRight:'5px'}}>Alt Kategoriler</Button> 
-        <Button sx={{border:'1px solid gray',borderRadius:'20px',width:'30%', backgroundColor:'white',color:'black',marginTop:'10px',}}>Ürünler</Button> 
-        {showCategories ? (
-            <Container maxWidth='lg' sx={{marginTop:'10px'}}>
-              <Grid container spacing={2} >
-          <Grid item xs={12} md={6} lg={4}>
-            <Category
-              image={Market}
-              tittle ="MARKET"
-            />
-          </Grid>
-          <Grid item  xs={12} md={6} lg={4}>
+    <div style={{ backgroundColor: 'rgb(218, 236, 237)' }} className='orderComponent '>
+      {/* Barkod girişi ve kategori butonları */}
+      <Input className='barcodeInput' sx={{ border: '1px solid black', borderRadius: '20px', backgroundColor: 'white', color: 'black', width: '90%', height: '50px', marginLeft: '10px', marginTop: '10px', fontSize: '20px', paddingLeft: '15px' }} disableUnderline='true' placeholder='Klavyeden Barkod Girişi'
+        endAdornment={
+          <InputAdornment position="start">
+            <CheckCircleIcon sx={{ color: 'black', fontSize: '50px' }} />
+          </InputAdornment>
+        }></Input>
+      <Button sx={{ border: '1px solid gray', borderRadius: '20px', width: '30%', backgroundColor: 'white', color: 'black', marginTop: '10px', marginRight: '5px', marginLeft: '3px' }} onClick={() => { setShowCategories(true) }}>Kategoriler</Button>
+      <Button sx={{ border: '1px solid gray', borderRadius: '20px', width: '30%', backgroundColor: 'white', color: 'black', marginTop: '10px', marginRight: '5px' }}>Alt Kategoriler</Button>
+      <Button sx={{ border: '1px solid gray', borderRadius: '20px', width: '30%', backgroundColor: 'white', color: 'black', marginTop: '10px', }}>Ürünler</Button>
+      {/* Kategoriler veya ürünler */}
+      {showCategories ? (
+        // Kategorileri göster
+        <Container maxWidth='lg' sx={{ marginTop: '10px' }}>
+          <Grid container spacing={2} >
+            <Grid onClick={() => showProducts("Market")} item xs={12} md={6} lg={4}>
+              <Category
+                image={Market}
+                tittle="MARKET"
+              />
+            </Grid>
+            <Grid onClick={()=>showProducts("home&clean")} item  xs={12} md={6} lg={4}>
           <Category
             image={Clean}
             tittle ="EV&TEMİZLİK"
           />
           </Grid>
-          <Grid item  xs={12} md={6} lg={4}>
+          <Grid onClick={()=>showProducts("Kitchen")} item  xs={12} md={6} lg={4}>
           <Category
           image={HomeKitchen}
-          tittle ="EV&MUTFAK GEREÇLERİ"
+          tittle ="EV&MUTFAK"
           />
           </Grid>
-          <Grid item  xs={12} md={6} lg={4}>
+          <Grid onClick={()=>showProducts("clothing&accessory")} item  xs={12} md={6} lg={4}>
           <Category
           image={Clothes}
           tittle ="GİYİM&AKSESUAR"
           />
           </Grid>
-          <Grid item  xs={12} md={6} lg={4}>
+          <Grid onClick={()=>showProducts("home&life")} item  xs={12} md={6} lg={4}>
           <Category
           image={HomeLife}
           tittle ="EV&YAŞAM"
           />
           </Grid>
-          <Grid item  xs={12} md={6} lg={4}>
+          <Grid onClick={()=>showProducts("book&stationary")} item  xs={12} md={6} lg={4}>
           <Category
           image={BookLibrary}
           tittle ="KİTAP&KIRTASİYE"
           />
           </Grid>
-        </Grid>
-            </Container>
-          
-        ) :(
-            <p></p>
-        )}
+          <Grid onClick={()=>showProducts("withoutBarcode")} item  xs={12} md={6} lg={4}>
+          <Category
+          image={barkodsuz}
+          tittle ="BARKODSUZ"
+          />
+          </Grid>
+          </Grid>
+        </Container>
+      ) : (
+        // Ürünleri göster
+        <Products 
+          names={productData.names}
+          prices={productData.prices}
+          codes={productData.codes}
+          ctgry ={selectedCategory}
+        />
+      )}
     </div>
   )
 }
 
-export default SelectProduct
+export default SelectProduct;
