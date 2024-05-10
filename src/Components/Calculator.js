d6bb4811845d50a9dcba5242009132bfb93ef31f
İsa ŞENGÜL <sengulisa49@icloud.com>
2024-05-11 02:45:17 +0300

    Taking a value of Button which give the amount of product

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -4,10 +4,11 @@ import { Container, Button, Input,InputAdornment } from '@mui/material';
 import RemoveIcon from '@mui/icons-material/Remove';
 import ClearIcon from '@mui/icons-material/Clear';
 import BackspaceIcon from '@mui/icons-material/Backspace';
-import { useProductCode } from '../Context/ProductContext';
+import { useState } from 'react';
+
 
 const Calculator = () => {
-   var {setProductPrice,ProductPrice} = useProductCode();
+   const [value, setValue] = useState('');
   return (
     <div className='orderComponent'>
       <Container sx={{marginTop:'10px'}}>
@@ -24,38 +25,38 @@ const Calculator = () => {
         <Button sx={{border:'1px solid blue', borderRadius:'20px',backgroundColor:'blue',color:'white',width:'35%',height:'40px',marginLeft:'10px',fontFamily:'inherit',fontSize:'15px'}} >TAKSITLI</Button>
       </Container >
       <Container sx={{marginTop:'5px'}}>
-        <Input disableUnderline='true' placeholder='0' value={ProductPrice} onChange={(e)=>{setProductPrice(e.target.value)}} sx={{width:'80%',height:'50px',border:'1px solid gray',borderRadius:'15px',backgroundColor:'white',padding:'10px'}}
+        <Input disableUnderline='true' placeholder='0' value={value} onChange={(e)=>{setValue(e.target.value)}} sx={{width:'80%',height:'50px',border:'1px solid gray',borderRadius:'15px',backgroundColor:'white',padding:'10px'}}
             endAdornment=
             {
               <InputAdornment position="end">
-                <ClearIcon sx={{marginRight:'20px',color:'black', fontSize:'40px'}}/>
+                <Button onClick={()=>setValue('')}><ClearIcon sx={{marginRight:'20px',color:'black', fontSize:'40px'}}/></Button>
               </InputAdornment>
             }
         ></Input>
-        <Button sx={{width:'18%',height:'50px',border:'1px solid gray',borderRadius:'15px',backgroundColor:'white',marginLeft:'3px'}}><RemoveIcon sx={{color:'black',fontSize:'50px'}}/></Button>
+        <Button onClick={e=> setValue(value.slice(0,-1))} sx={{width:'18%',height:'50px',border:'1px solid gray',borderRadius:'15px',backgroundColor:'white',marginLeft:'3px'}}><RemoveIcon sx={{color:'black',fontSize:'50px'}}/></Button>
       </Container>
       <Container sx={{marginTop:'5px'}}>
-      <Button sx={{border:'1px solid darkblue', borderRadius:'20px',backgroundColor:'darkblue',color:'white',width:'30%',height:'50px',marginRight:'5px',fontFamily:'inherit',fontSize:'25px'}}  >00</Button>
-        <Button sx={{border:'1px solid darkblue', borderRadius:'20px',backgroundColor:'darkblue',color:'white',width:'30%',height:'50px',fontFamily:'inherit',fontSize:'25px'}}  ><BackspaceIcon fontSize='large'/></Button>
+      <Button onClick={e=> setValue( value + e.target.value) } sx={{border:'1px solid darkblue', borderRadius:'20px',backgroundColor:'darkblue',color:'white',width:'30%',height:'50px',marginRight:'5px',fontFamily:'inherit',fontSize:'25px'}} value={'00'} >00</Button>
+        <Button onClick={e=> setValue(value.slice(0,-1))} sx={{border:'1px solid darkblue', borderRadius:'20px',backgroundColor:'darkblue',color:'white',width:'30%',height:'50px',fontFamily:'inherit',fontSize:'25px'}}  ><BackspaceIcon fontSize='large'/></Button>
         <Button sx={{border:'1px solid green', borderRadius:'20px',backgroundColor:'green',color:'white',width:'35%',height:'50px',marginLeft:'5px',fontFamily:'inherit',fontSize:'15px'}} >KAMPANYA LISTESI</Button>
       </Container >
       <Container sx={{marginTop:'5px'}}>
-      <Button sx={{border:'1px solid darkblue', borderRadius:'20px',backgroundColor:'darkblue',color:'white',width:'20%',height:'50px',marginRight:'5px',fontFamily:'inherit',fontSize:'25px'}}  >7</Button>
-        <Button sx={{border:'1px solid darkblue', borderRadius:'20px',backgroundColor:'darkblue',color:'white',width:'20%',height:'50px',fontFamily:'inherit',fontSize:'25px',marginRight:'5px'}}  >8</Button>
-        <Button sx={{border:'1px solid darkblue', borderRadius:'20px',backgroundColor:'darkblue',color:'white',width:'20%',height:'50px',fontFamily:'inherit',fontSize:'25px'}}  >9</Button>
+      <Button onClick={e=> setValue( value + e.target.value) } sx={{border:'1px solid darkblue', borderRadius:'20px',backgroundColor:'darkblue',color:'white',width:'20%',height:'50px',marginRight:'5px',fontFamily:'inherit',fontSize:'25px'}} value={'7'}>7</Button>
+        <Button onClick={e=> setValue( value + e.target.value) } sx={{border:'1px solid darkblue', borderRadius:'20px',backgroundColor:'darkblue',color:'white',width:'20%',height:'50px',fontFamily:'inherit',fontSize:'25px',marginRight:'5px'}} value={'8'}  >8</Button>
+        <Button onClick={e=> setValue( value + e.target.value) } sx={{border:'1px solid darkblue', borderRadius:'20px',backgroundColor:'darkblue',color:'white',width:'20%',height:'50px',fontFamily:'inherit',fontSize:'25px'}} value={'9'} >9</Button>
         <Button sx={{border:'1px solid red', borderRadius:'20px',backgroundColor:'red',color:'white',width:'35%',height:'50px',marginLeft:'5px',fontFamily:'inherit',fontSize:'15px'}} >MIKTAR</Button>
       </Container >
       <Container sx={{marginTop:'5px',display:'flex',flexDirection:'row'}} >
       <div>
         <div>
-        <Button sx={{border:'1px solid darkblue', borderRadius:'20px',backgroundColor:'darkblue',color:'white',width:'90px',height:'50px',marginRight:'5px',fontFamily:'inherit',fontSize:'25px'}}  >4</Button>
-        <Button sx={{border:'1px solid darkblue', borderRadius:'20px',backgroundColor:'darkblue',color:'white',width:'90px',height:'50px',fontFamily:'inherit',fontSize:'25px',marginRight:'5px'}}  >5</Button>
-        <Button sx={{border:'1px solid darkblue', borderRadius:'20px',backgroundColor:'darkblue',color:'white',width:'95px',height:'50px',fontFamily:'inherit',fontSize:'25px'}}  >6</Button>
+        <Button onClick={e=> setValue( value + e.target.value) } sx={{border:'1px solid darkblue', borderRadius:'20px',backgroundColor:'darkblue',color:'white',width:'90px',height:'50px',marginRight:'5px',fontFamily:'inherit',fontSize:'25px'}} value={'4'} >4</Button>
+        <Button onClick={e=> setValue( value + e.target.value) } sx={{border:'1px solid darkblue', borderRadius:'20px',backgroundColor:'darkblue',color:'white',width:'90px',height:'50px',fontFamily:'inherit',fontSize:'25px',marginRight:'5px'}} value={'5'} >5</Button>
+        <Button onClick={e=> setValue( value + e.target.value) } sx={{border:'1px solid darkblue', borderRadius:'20px',backgroundColor:'darkblue',color:'white',width:'95px',height:'50px',fontFamily:'inherit',fontSize:'25px'}} value={'6'} >6</Button>
         </div>
         <div style={{marginTop:'5px'}}>
-        <Button sx={{border:'1px solid darkblue', borderRadius:'20px',backgroundColor:'darkblue',color:'white',width:'90px',height:'50px',marginRight:'5px',fontFamily:'inherit',fontSize:'25px'}}  >1</Button>
-        <Button sx={{border:'1px solid darkblue', borderRadius:'20px',backgroundColor:'darkblue',color:'white',width:'90px',height:'50px',fontFamily:'inherit',fontSize:'25px',marginRight:'5px'}}  >2</Button>
-        <Button sx={{border:'1px solid darkblue', borderRadius:'20px',backgroundColor:'darkblue',color:'white',width:'95px',height:'50px',fontFamily:'inherit',fontSize:'25px'}}  >3</Button>
+        <Button onClick={e=> setValue( value + e.target.value) } sx={{border:'1px solid darkblue', borderRadius:'20px',backgroundColor:'darkblue',color:'white',width:'90px',height:'50px',marginRight:'5px',fontFamily:'inherit',fontSize:'25px'}} value={'1'} >1</Button>
+        <Button onClick={e=> setValue( value + e.target.value) } sx={{border:'1px solid darkblue', borderRadius:'20px',backgroundColor:'darkblue',color:'white',width:'90px',height:'50px',fontFamily:'inherit',fontSize:'25px',marginRight:'5px'}} value={'2'} >2</Button>
+        <Button onClick={e=> setValue( value + e.target.value) } sx={{border:'1px solid darkblue', borderRadius:'20px',backgroundColor:'darkblue',color:'white',width:'95px',height:'50px',fontFamily:'inherit',fontSize:'25px'}} value={'3'} >3</Button>
         </div>
       </div>
       <div>
@@ -63,9 +64,9 @@ const Calculator = () => {
       </div> 
       </Container>
       <Container sx={{marginTop:'5px'}}>
-      <Button sx={{border:'1px solid darkblue', borderRadius:'20px',backgroundColor:'darkblue',color:'white',width:'135px',height:'50px',marginRight:'5px',fontFamily:'inherit',fontSize:'25px',}}  >0</Button>
-        <Button sx={{border:'1px solid darkblue', borderRadius:'20px',backgroundColor:'darkblue',color:'white',width:'150px',height:'50px',fontFamily:'inherit',fontSize:'25px',marginRight:'5px'}}  >.</Button>
-        <Button sx={{border:'1px solid darkblue', borderRadius:'20px',backgroundColor:'darkblue',color:'white',width:'35%',height:'50px',marginLeft:'5px',fontFamily:'inherit',fontSize:'15px'}} >GIRIS</Button>
+      <Button onClick={e=> setValue( value + e.target.value) } sx={{border:'1px solid darkblue', borderRadius:'20px',backgroundColor:'darkblue',color:'white',width:'135px',height:'50px',marginRight:'5px',fontFamily:'inherit',fontSize:'25px',}} value={'0'} >0</Button>
+        <Button onClick={e=> setValue( value + e.target.value) } sx={{border:'1px solid darkblue', borderRadius:'20px',backgroundColor:'darkblue',color:'white',width:'150px',height:'50px',fontFamily:'inherit',fontSize:'25px',marginRight:'5px'}} value={'.'}  >.</Button>
+        <Button sx={{border:'1px solid darkblue', borderRadius:'20px',backgroundColor:'darkblue',color:'white',width:'30%',height:'50px',marginLeft:'5px',fontFamily:'inherit',fontSize:'15px'}} >GIRIS</Button>
       </Container>
     </div>
   )
