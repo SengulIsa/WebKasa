a97db332663dc1c51a88e5818d6a9ff5fb521581
İsa ŞENGÜL <sengulisa49@icloud.com>
2024-05-12 16:02:40 +0300

    check the price information for does it come to orderedPrduct component or not

--- src/Components/OrderedProducts.js
+++ src/Components/OrderedProducts.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-const OrderedProducts = ({names,prices}) => {
+const OrderedProducts = ({ names, prices }) => {
   return (
     <div>
-        sdvöcxsasçödl
+      {prices.map((price, index) => (
+        <div key={index}>
+          <p>{index + 1}. price: {price}</p>
+        </div>
+      ))}
     </div>
-  )
+  );
 };
 
-export default OrderedProducts
+export default OrderedProducts;
