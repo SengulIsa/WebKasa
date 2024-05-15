b01a3610c0e25a22144ae345a51364c919864ba8
İsa ŞENGÜL <sengulisa49@icloud.com>
2024-05-15 03:34:04 +0300

    Changed the totalValue For subtotal value with using useeffect hooks,some edition for Ordered Product information

--- src/Components/OrderedProducts.js
+++ src/Components/OrderedProducts.js
@@ -1,11 +1,38 @@
-import React, { useState, useEffect } from 'react';
+
+import React, {useEffect } from 'react';
+import { useProductCode } from '../Context/ProductContext';
+import { Container, Typography } from '@mui/material';
 
 const OrderedProducts = ({ names, prices }) => {
+  const { Amounts,setTotalValue } = useProductCode();
+  
+
+  useEffect(() => {
+    // Her bir ürünün fiyatı ve miktarını çarparak toplam değeri hesapla
+    let sum = 0;
+    prices.forEach((price, index) => {
+      sum += price * Amounts[index];
+    });
+    setTotalValue(sum);
+  }, [prices, Amounts]);
+
   return (
     <div>
       {prices.map((price, index) => (
-        <div key={index}>
-          <p>{index + 1}. price: {price}</p>
+  <div key={index}>
+     <Container sx={{backgroundColor:'yellow',border:'1px solid gray', borderRadius:'10px',marginBottom:'5px',marginTop:'5px',display:'flex',flexDirection:'row',justifyContent:'space-between'}}>
+        <Typography>
+          <Typography>KDV %1</Typography> 
+          <Typography>{names[index]}</Typography>
+        </Typography>
+        <Typography>
+           {/* Amounts.length > index && */}
+        {<Typography>{Amounts[index]} pieces</Typography>}
+         <Typography>{price * Amounts[index]} TL</Typography>
+         
+        </Typography>
+          </Container>
+          
         </div>
       ))}
     </div>
