fedfe8643a38b43a29c6db741cd1c533b27437cc
İsa ŞENGÜL <sengulisa49@icloud.com>
2024-05-22 20:24:00 +0300

    Creating PaymentPage and it's Components

---
+++ src/Components/PaymentCalculator.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PaymentClaculator = () => {
+  return (
+    <div>PaymentClaculator</div>
+  )
+}
+
+export default PaymentClaculator
---
+++ src/Components/PaymentOperations.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PaymentOperations = () => {
+  return (
+    <div>PaymentOperations</div>
+  )
+}
+
+export default PaymentOperations
