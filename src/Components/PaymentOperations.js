f81bf39773ab475acfad6dd64932a0270f5bd85c
İsa ŞENGÜL <sengulisa49@icloud.com>
2024-05-29 02:02:04 +0300

    Added operation on the button of paymentCalculator page

--- src/Components/PaymentOperations.js
+++ src/Components/PaymentOperations.js
@@ -1,67 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Styles/PaymentPage.css';
-import { Button,Container, Typography } from '@mui/material';
+import { Button, Container, Typography, Modal } from '@mui/material';
 import CropSquareIcon from '@mui/icons-material/CropSquare';
+import Invoice from './Invoice'; 
 
 const PaymentOperations = () => {
+ const [openInvoice, setOpenInvoice] = useState(false);
+
+  const handleOpenInvoice = () => {
+      setOpenInvoice(true);
+  };
+
+  const handleCloseInvoice = () => {
+    setOpenInvoice(false);
+  };
+
   return (
     <div className='PaymentCmpnts'>
-    <Container>
-      <Button 
-        sx={{
-          border:'1px solid blue',
-          borderRadius:'20px',
-          width:'100%',height:'60px',
-          marginBottom:'5px',marginTop:'10px',
-          backgroundColor:'blue', color:'white',fontSize:'17px',fontFamily:'fantasy'
+      <Container>
+        <Button 
+          sx={{
+            border:'1px solid blue',
+            borderRadius:'20px',
+            width:'100%',height:'60px',
+            marginBottom:'5px',marginTop:'10px',
+            backgroundColor:'blue', color:'white',fontSize:'17px',fontFamily:'fantasy'
           }}>
             <Typography>HADİ CÜZDAN</Typography>
-      </Button>
-      <Button
-            sx={{
-              border:'1px solid blue',
-              borderRadius:'20px',
-              width:'100%',height:'60px',
-              marginBottom:'5px',
-              backgroundColor:'blue', color:'white',fontSize:'17px',fontFamily:'fantasy'
-              }}>
-                <Typography>TOMBANK CÜZDAN</Typography>
-      </Button>
-      <Button
-             sx={{
-              border:'1px solid brown',
-              borderRadius:'20px',
-              width:'100%',height:'60px',
-              marginBottom:'5px',
-              backgroundColor:'brown', color:'white',fontSize:'17px',fontFamily:'fantasy'
-              }}>
-                <Typography>E-FATURA</Typography>
-      </Button>
-      <Button
-              sx={{
-                border:'1px solid green',
-                borderRadius:'20px',
-                width:'60%',height:'60px',
-                marginBottom:'5px',
-                backgroundColor:'green', color:'white',fontSize:'17px',fontFamily:'fantasy'
-                }}>
-                  <Typography>HEDİYE ÇEKİ(KIRMIZI KART)</Typography>
-      </Button>
-    </Container>
-    <Container sx={{marginTop:'90px'}}>
-      <Button
+        </Button>
+        <Button
+          sx={{
+            border:'1px solid blue',
+            borderRadius:'20px',
+            width:'100%',height:'60px',
+            marginBottom:'5px',
+            backgroundColor:'blue', color:'white',fontSize:'17px',fontFamily:'fantasy'
+          }}>
+            <Typography>TOMBANK CÜZDAN</Typography>
+        </Button>
+        <Button
+          onClick={handleOpenInvoice}
+          sx={{
+            border:'1px solid brown',
+            borderRadius:'20px',
+            width:'100%',height:'60px',
+            marginBottom:'5px',
+            backgroundColor:'brown', color:'white',fontSize:'17px',fontFamily:'fantasy'
+          }}>
+            <Typography>E-FATURA</Typography>
+        </Button>
+        <Button
+          sx={{
+            border:'1px solid green',
+            borderRadius:'20px',
+            width:'60%',height:'60px',
+            marginBottom:'5px',
+            backgroundColor:'green', color:'white',fontSize:'17px',fontFamily:'fantasy'
+          }}>
+            <Typography>HEDİYE ÇEKİ(KIRMIZI KART)</Typography>
+        </Button>
+      </Container>
+      <Container sx={{marginTop:'90px'}}>
+        <Button
           sx={{
             border:'1px solid blue',
             borderRadius:'20px',
             width:'100%',height:'60px',
             color:'blue'
-          }}
-     
-      >  <CropSquareIcon fontSize='small'/> <Typography>E-ARŞİV FATURA KAREKOD</Typography></Button>
-    </Container>
-      
+          }}>
+          <CropSquareIcon fontSize='small'/> 
+          <Typography>E-ARŞİV FATURA KAREKOD</Typography>
+        </Button>
+      </Container>
+
+      <Modal open={openInvoice} onClose={handleCloseInvoice}>
+        <Invoice setOpenInvoice={setOpenInvoice} />
+      </Modal>
     </div>
-  )
+  );
 }
 
-export default PaymentOperations
+export default PaymentOperations;
