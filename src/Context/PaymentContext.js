0a1d8283bb42456d8a2cd6ca9bfca37f0edabd79
İsa ŞENGÜL <sengulisa49@icloud.com>
2024-05-29 02:05:30 +0300

    added paymentContext file as context api structure for sharing payment values all  application

---
+++ src/Context/PaymentContext.js
@@ -0,0 +1,28 @@
+import React,{ createContext, useContext, useState } from 'react';
+
+const PaymentContext = createContext();
+
+export const PaymentProvider = ({ children }) => {
+    const [isPaymentSlipOpen, setIsPaymentSlipOpen] = useState(false);
+    const [paymentCount, setPaymentCount] = useState(0);
+    const [receivedMoney, setReceivedMoney] = useState(0);
+    const [change, setChange] = useState(0.00);
+    const [paymentType,setPaymentType]= useState([]);
+
+  const contextValue = {
+   isPaymentSlipOpen,setIsPaymentSlipOpen,
+   paymentCount, setPaymentCount,
+   receivedMoney, setReceivedMoney,
+   change, setChange,
+   paymentType,setPaymentType,
+  }
+  return (
+    <PaymentContext.Provider value={contextValue}>
+      {children}
+    </PaymentContext.Provider>
+  );
+};
+
+export const usePaymentInfo = () => {
+  return useContext(PaymentContext);
+};
